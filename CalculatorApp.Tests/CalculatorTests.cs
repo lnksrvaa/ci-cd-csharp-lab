21fe6af5e08f71770d0244ae49cdc2592057ce6b
lnksrvaa <alena.kos2007@gmail.com>
2026-05-14 00:49:08 +0300

    Add files via upload

---
+++ CalculatorApp.Tests/CalculatorTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+using CalculatorApp;
+
+namespace CalculatorApp.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Add_ReturnsCorrectSum()
+        {
+            Calculator calc = new Calculator();
+
+            int result = calc.Add(2, 3);
+
+            Assert.Equal(5, result);
+        }
+    }
+}
